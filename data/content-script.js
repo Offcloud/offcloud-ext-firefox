a9c8f4e702059cef0220fe5523f1225a87e5ea8c
Hoat Le <hoatle@teracy.com>
2014-10-08 09:14:15 +0000

    CLT-314 | Apply feedback

--- data/content-script.js
+++ data/content-script.js
@@ -1,5 +1,17 @@
+function getParentLink(node) {
+    // dirty way to find anchor parents, for firefox
+    var parent = node.parentNode;
+    if(parent.parentNode && parent.parentNode.href) {
+        parent = parent.parentNode;
+    } else if (parent.parentNode && parent.parentNode.parentNode && parent.parentNode.href) {
+        parent = parent.parentNode.parentNode;
+    }
+    return parent.href;
+}
+
 self.on("context", function (node) {
-    if (node.href || window.getSelection().toString().length > 0) {
+    var href = node.href || getParentLink(node);
+    if (href || window.getSelection().toString().length > 0) {
     	return true;
     }
 });
